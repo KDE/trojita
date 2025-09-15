a7fb418b196cbdfb3cd919c1f8a236a45f924921
Johnny Jazeix <jazeix@gmail.com>
2025-09-15 08:57:34 +0200

    update Messages.sh to use scripty macros
    BUG: 431128

--- Messages.sh
+++ Messages.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-# xgettext-generated .po files use different context than QObject::tr.
-# The generated files use something like a file name while QObject::tr expects
-# class names. This approach works.
+$EXTRACT_TR_STRINGS `find src/ \
+                          -name \*.cpp -o \
+                          -name \*.h -o \
+                          -name \*.ui` \
+                    -o $podir/trojita_common.pot
 
-rm -f "${podir}/trojita.ts"
-lupdate -silent -recursive src/ -ts "${podir}/trojita.ts"
-lconvert "${podir}/trojita.ts" --sort-contexts --output-format pot -o "${podir}/trojita_common.pot"
-rm "${podir}/trojita.ts"
