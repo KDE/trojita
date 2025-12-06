f0b92d73aadbc5ffa218d95df512d3eef6e49be0
Espen Sandøy Hustad <espen@ehustad.com>
2025-12-06 20:34:13 +0100

    Mark overriding methods with override
    Fix clang warnings (-Winconsistent-missing-override).

--- src/Gui/AttachmentView.h
+++ src/Gui/AttachmentView.h
@@ -66,9 +66,9 @@ public:
     void zoomOut() override;
     void zoomOriginal() override;
 protected:
-    virtual void mouseMoveEvent(QMouseEvent *event);
-    virtual void mousePressEvent(QMouseEvent *event);
-    virtual void paintEvent(QPaintEvent *event);
+    virtual void mouseMoveEvent(QMouseEvent *event) override;
+    virtual void mousePressEvent(QMouseEvent *event) override;
+    virtual void paintEvent(QPaintEvent *event) override;
 private slots:
     void slotDownloadAttachment();
     void slotOpenAttachment();
